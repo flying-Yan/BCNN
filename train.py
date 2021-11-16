f3307c980b55ec93b33b0dcbf8736d78887bd715
flying-Yan <56363942+flying-Yan@users.noreply.github.com>
2021-11-16 12:29:26 -0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,241 @@
+import argparse
+import os
+import time
+import logging
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import models
+from torch.autograd import Variable
+from data import get_dataset
+from preprocess import get_transform
+from utils import *
+from datetime import datetime
+
+from models.cb_nin import Net
+
+
+parser = argparse.ArgumentParser(description='PyTorch ConvNet Training')
+parser.add_argument('--input_size', type=int, default=32,
+                    help='image input size')
+parser.add_argument('--dataset', metavar='DATASET', default='cifar10',
+                    help='dataset name or folder')
+parser.add_argument('--type', default='torch.cuda.FloatTensor',
+                    help='type of tensor - e.g torch.cuda.HalfTensor')
+parser.add_argument('--gpus', default='0',
+                    help='gpus used for training - e.g 0,1,3')
+parser.add_argument('-j', '--workers', default=8, type=int, metavar='N',
+                    help='number of data loading workers (default: 8)')
+parser.add_argument('--epochs', default=300, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('-b', '--batch-size', default=128, type=int,
+                    metavar='N', help='mini-batch size (default: 128)')
+parser.add_argument('--optimizer', default='SGD', type=str, metavar='OPT',
+                    help='optimizer function used')
+parser.add_argument('--lr', '--learning_rate', default=0.01, type=float,
+                    metavar='LR', help='initial learning rate')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--weight-decay', '--wd', default=0, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)')
+parser.add_argument('--print-freq', '-p', default=10, type=int,
+                    metavar='N', help='print frequency (default: 10)')
+parser.add_argument('--save', metavar='SAVE', default='./result/NIN_cc',
+                    help='saved folder')
+
+
+def main():
+
+
+    global args, best_prec
+    best_prec1 = 0
+    args = parser.parse_args()
+    
+    
+    save_path = os.path.join(args.save)
+
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    setup_logging(os.path.join(save_path, 'log.txt'))
+    results_file = os.path.join(save_path, 'results.%s')
+    results = ResultsLog(results_file % 'csv', results_file % 'html')
+
+
+    logging.info("saving to %s", save_path)
+    logging.debug("run arguments: %s", args)
+
+    
+    
+    if 'cuda' in args.type:
+        args.gpus = [int(i) for i in args.gpus.split(',')]
+        torch.cuda.set_device(args.gpus[0])
+        cudnn.benchmark = True
+    else:
+        args.gpus = None
+
+    
+    model = Net()
+
+    
+    # Data loading code
+    default_transform = {
+        'train': get_transform(args.dataset,
+                               input_size=args.input_size, augment=True),
+        'eval': get_transform(args.dataset,
+                              input_size=args.input_size, augment=False)
+    }
+    transform = getattr(model, 'input_transform', default_transform)
+    regime = getattr(model, 'regime', {0: {'optimizer': args.optimizer,
+                                           'lr': args.lr,
+                                           'momentum': args.momentum,
+                                           'weight_decay': args.weight_decay}})
+    
+    
+    criterion = getattr(model, 'criterion', nn.CrossEntropyLoss)()
+    criterion.type(args.type)
+    model.type(args.type)
+    
+    num_parameters = sum([l.nelement() for l in model.parameters()])
+    logging.info("number of parameters: %d", num_parameters)
+
+    val_data = get_dataset(args.dataset, 'val', transform['eval'])
+    val_loader = torch.utils.data.DataLoader(
+        val_data,
+        batch_size=args.batch_size, shuffle=False,
+        num_workers=args.workers, pin_memory=True)
+
+
+    
+    train_data = get_dataset(args.dataset, 'train', transform['train'])
+    train_loader = torch.utils.data.DataLoader(
+        train_data,
+        batch_size=args.batch_size, shuffle=True,
+        num_workers=args.workers, pin_memory=True)
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr)
+    logging.info('training regime: %s', regime)
+
+
+    for epoch in range(args.epochs):
+        optimizer = adjust_optimizer(optimizer, epoch, regime)
+
+        train_loss, train_prec1, train_prec5 = train(
+            train_loader, model, criterion, epoch, optimizer)
+
+
+        val_loss, val_prec1, val_prec5 = validate(
+            val_loader, model, criterion, epoch)
+
+        is_best = val_prec1 > best_prec1
+        best_prec1 = max(val_prec1, best_prec1)
+
+
+        save_checkpoint({
+            'epoch': epoch + 1,
+            'state_dict': model.state_dict(),
+            'best_prec1': best_prec1,
+            'regime': regime
+        }, is_best, path=save_path)
+        logging.info('\n Epoch: {0}\t'
+                     'Training Loss {train_loss:.4f} \t'
+                     'Training Prec@1 {train_prec1:.3f} \t'
+                     'Training Prec@5 {train_prec5:.3f} \t'
+                     'Validation Loss {val_loss:.4f} \t'
+                     'Validation Prec@1 {val_prec1:.3f} \t'
+                     'Validation Prec@5 {val_prec5:.3f} \t'
+                     'best_prec {best_prec1:.3f} \n'
+
+                     .format(epoch + 1, train_loss=train_loss, val_loss=val_loss,
+                             train_prec1=train_prec1, val_prec1=val_prec1,
+                             train_prec5=train_prec5, val_prec5=val_prec5, best_prec1 = best_prec1))
+
+        results.add(epoch=epoch + 1, train_loss=train_loss, val_loss=val_loss,
+                    train_error1=100 - train_prec1, val_error1=100 - val_prec1,
+                    train_error5=100 - train_prec5, val_error5=100 - val_prec5)
+        results.save()
+
+
+def forward(data_loader, model, criterion, epoch=0, training=True, optimizer=None):
+    if args.gpus and len(args.gpus) > 1:
+        model = torch.nn.DataParallel(model, args.gpus)
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    end = time.time()
+    for i, (inputs, target) in enumerate(data_loader):
+        data_time.update(time.time() - end)
+        if args.gpus is not None:
+            target = target.cuda()
+        input_var = Variable(inputs.type(args.type), volatile=not training)
+        target_var = Variable(target)
+
+        # compute output
+        output = model(input_var)
+        loss = criterion(output, target_var)
+        if type(output) is list:
+            output = output[0]
+
+        # measure accuracy and record loss
+        prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
+
+        losses.update(loss.data.item(), inputs.size(0))
+        top1.update(prec1.item(), inputs.size(0))
+        top5.update(prec5.item(), inputs.size(0))
+
+        if training:
+            # compute gradient and do SGD step
+            optimizer.zero_grad()
+            loss.backward()
+            for p in list(model.parameters()):
+                if hasattr(p,'org'):
+                    p.data.copy_(p.org)
+            optimizer.step()
+            for p in list(model.parameters()):
+                if hasattr(p,'org'):
+                    p.org.copy_(p.data.clamp_(-1.3,1.3))
+
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_freq == 0:
+            logging.info('{phase} - Epoch: [{0}][{1}/{2}]\t'
+                         'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                         'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                         'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                         'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                             epoch, i, len(data_loader),
+                             phase='TRAINING' if training else 'EVALUATING',
+                             batch_time=batch_time,
+                             loss=losses, top1=top1, top5=top5))
+
+    return losses.avg, top1.avg, top5.avg
+
+
+def train(data_loader, model, criterion, epoch, optimizer):
+    # switch to train mode
+    model.train()
+    return forward(data_loader, model, criterion, epoch,
+                   training=True, optimizer=optimizer)
+
+
+def validate(data_loader, model, criterion, epoch):
+    # switch to evaluate mode
+    model.eval()
+    return forward(data_loader, model, criterion, epoch,
+                   training=False, optimizer=None)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
